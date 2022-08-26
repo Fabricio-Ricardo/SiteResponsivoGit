5b8efd7daa4ce3e12b335a19d606caec64e37d56
Fabricio-Ricardo <fabricioricardo947@gmail.com>
2022-08-26 17:49:23 -0300

    update scroll

---
+++ scroll.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal({ reset: true })
+ScrollReveal().reveal('.contTexto',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0}
+})
+
+ScrollReveal().reveal('.contFoto',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0, duration: 800}
+})
+
+ScrollReveal().reveal('.contTexto2',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0, duration: 800}
+})
+ScrollReveal().reveal('.contTexto3',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0, duration: 800}
+})
+ScrollReveal().reveal('.contFoto2',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0, duration: 800}
+})
+ScrollReveal().reveal('.contTexto4',{
+    delay: 900,
+    rotate:{x:100, y:0, z:0, duration: 800}
+})
